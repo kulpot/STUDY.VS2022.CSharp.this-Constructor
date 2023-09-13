30f8512da0162de3a6fc62c5b843ae960600946f
Kulpot <kulpotwahu@gmail.com>
2023-09-13 23:28:53 +0800

    ..

--- this Constructor/Program.cs
+++ this Constructor/Program.cs
@@ -1,14 +1,30 @@
 ﻿using System;
 
 //ref link:https://www.youtube.com/watch?v=dWd7O7fffcs&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=56
-//
+// this() -- for overload of constructors
 
 class Cow
 {
     public string Name { get; set; }
-    public Cow(string name)
+    public int Age { get; set; }
+    
+    //public Cow(string name, int age = 0)
+    public Cow(string name) 
     {
+        Console.WriteLine("Cow(name)");
+        if (name.Length > 10)
+            throw new Exception();
         Name = name;
+        //Age = age;
+
+    }
+    public Cow(string name, int age) : this(name) // ------------
+    {
+        Console.WriteLine("Cow(name, age)");
+        //if(name.Length > 10)
+        //    throw new Exception();
+        //Name = name;
+        Age = age;
     }
 }
 
@@ -16,7 +32,8 @@ class Mainclass
 {
     static void Main()
     {
-        Cow cow = new Cow("Betsy");
+        Cow cow = new Cow("Betsy", 5);
         Console.WriteLine(cow.Name);
+        Console.WriteLine(cow.Age);
     }
 }
