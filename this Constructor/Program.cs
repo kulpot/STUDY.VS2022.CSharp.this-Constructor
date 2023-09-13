7c9acbf7c066ba1d467b2732a7df82102bc78ed5
Kulpot <kulpotwahu@gmail.com>
2023-09-13 23:19:22 +0800

    Add project files.

---
+++ this Constructor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+//ref link:https://www.youtube.com/watch?v=dWd7O7fffcs&list=PLRwVmtr-pp07XP8UBiUJ0cyORVCmCgkdA&index=56
+//
+
+class Cow
+{
+    public string Name { get; set; }
+    public Cow(string name)
+    {
+        Name = name;
+    }
+}
+
+class Mainclass
+{
+    static void Main()
+    {
+        Cow cow = new Cow("Betsy");
+        Console.WriteLine(cow.Name);
+    }
+}
